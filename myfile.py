98cfc63973f74d7f53205ddff217faf640ffed60
Chanukya <97583921+Chanukya113118@users.noreply.github.com>
2022-11-09 09:24:14 +0530

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -4,14 +4,14 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-data=pd.read_csv('data.csv')
-rfc=RandomForestClassifier()
-le=LabelEncoder()
-data['le']=le.fit_transform(data['label'])
-xtr,xtes,ytr,ytes=train_test_split(data.iloc[:,:7],data.iloc[:,-1])
-fit=rfc.fit(xtr,ytr)
-st.write(rfc.score(xtes,ytes))
 def input_fun():
+    data=pd.read_csv('data.csv')
+    rfc=RandomForestClassifier()
+    le=LabelEncoder()
+    data['le']=le.fit_transform(data['label'])
+    xtr,xtes,ytr,ytes=train_test_split(data.iloc[:,:7],data.iloc[:,-1])
+    fit=rfc.fit(xtr,ytr)
+    st.write(rfc.score(xtes,ytes))
     N=st.number_input('Enter Nitrogen Value')
     P=st.number_input('Enter Phosphorus Value')
     K=st.number_input('Enter Potassium Value')
